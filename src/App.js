6d6818386ae40e528e2ab1cb6006cfcb4a07d84e
cumPirate <crazyrealindividual123@gmail.com>
2023-03-04 10:19:19 +1400

    About Section Done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/header/Header';
-import Home from './components/home/Home';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 const App = () => {
   return (
     <>
       <Header />
-      
-      <main className='main'>
-      <Home />
+
+      <main className="main">
+        <Home />
+        <About />
       </main>
     </>
-  )
-
-}
+  );
+};
 
 export default App;
---
+++ src/components/skillls/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Skillls() {
+  return (
+    <div>
+      Skillls
+    </div>
+  );
+}
+
+export default Skillls;
